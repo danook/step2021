d9d8b9580e5acf54c101073c10d9fb4cab738bb1
MiCan <14_honokanada.130@ezweb.ne.jp>
2021-06-25 11:31:27 +0900

    week 7 introduced simulated annealing

--- week7/tsp/google-step-tsp/solver.cpp
+++ week7/tsp/google-step-tsp/solver.cpp
@@ -43,30 +43,27 @@ std::vector<int> get_greedy_tour(const std::vector<std::vector<double>> &distanc
 // returns a tour made by uncrossing the two edges.
 // Otherwise, returns the given tour.
 // NOTE: index1 must be smaller than index2.
-std::vector<int> uncross_edges(const std::vector<int> &tour, const int &index1, const int &index2, const std::vector<std::vector<double>> &distances)
+bool judge_uncross(std::vector<int> &tour, const int &index1, const int &index2, const std::vector<std::vector<double>> &distances)
 {
     int num_of_cities = distances.size();
     double length_of_edges = distances[tour[index1]][tour[(index1 + 1) % tour.size()]] + distances[tour[index2]][tour[(index2 + 1) % tour.size()]];
     double length_of_rearranged_edges = distances[tour[index1]][tour[index2]] + distances[tour[(index1 + 1) % tour.size()]][tour[(index2 + 1) % tour.size()]];
-    std::vector<int> new_tour(tour);
 
-    if (length_of_rearranged_edges < length_of_edges) // When two edges are crossed
-    {
-        std::reverse(new_tour.begin() + index1 + 1, new_tour.begin() + index2 + 1);
-    }
-
-    assert(get_score(new_tour, distances) <= get_score(tour, distances));
-    assert(check_tour(new_tour, num_of_cities));
-    return new_tour;
+    return length_of_rearranged_edges < length_of_edges; // When two edges are crossed
 }
 
-std::vector<int> two_opt(const std::vector<int> &tour, const std::vector<std::vector<double>> &distances, const double &time_in_second)
+std::vector<int> &uncross_edges(std::vector<int> &tour, const int &index1, const int &index2)
+{
+    std::reverse(tour.begin() + index1 + 1, tour.begin() + index2 + 1);
+    return tour;
+}
+
+std::vector<int> &two_opt(std::vector<int> &tour, const std::vector<std::vector<double>> &distances, const double &time_in_second)
 {
     int num_of_cities = distances.size();
 
     std::random_device seed_gen;
     std::mt19937 random_engine(seed_gen());
-    std::vector<int> new_tour(tour);
 
     // Choose two edges at random, and uncross them if they are crossed.
     std::time_t start = std::time(NULL);
@@ -74,19 +71,31 @@ std::vector<int> two_opt(const std::vector<int> &tour, const std::vector<std::ve
     {
         // Choose index1 and index2 at random.
         std::pair<int, int> indices = gen_random_indices(num_of_cities, random_engine);
-        new_tour = uncross_edges(new_tour, indices.first, indices.second, distances);
+        if (judge_uncross(tour, indices.first, indices.second, distances))
+        {
+            tour = uncross_edges(tour, indices.first, indices.second);
+        }
+        // TODO: Introduce SA here!
     }
 
-    assert(check_tour(new_tour, num_of_cities));
-    return new_tour;
+    assert(check_tour(tour, num_of_cities));
+    return tour;
 }
 
-// NOTE: index1 must be smaller than index2.
-std::vector<int> insert_subsequence(const std::vector<int> &main_tour, const std::vector<int> &sub_sequence, const std::vector<std::vector<double>> &distances)
+struct InsertPlace
 {
-    int num_of_cities = distances.size();
+    int index;
+    bool reverses;
+    InsertPlace(int index0, bool reverses0)
+    {
+        index = index0;
+        reverses = reverses0;
+    }
+};
+
+InsertPlace get_place_to_insert_subsequence(const std::vector<int> &main_tour, const std::vector<int> &subsequence, const std::vector<std::vector<double>> &distances)
+{
     int main_tour_size = main_tour.size();
-    std::vector<int> new_tour;
     int min_edge_start = -1;
     int min_edge_end = -1;
     double min_distance = -1;
@@ -97,24 +106,32 @@ std::vector<int> insert_subsequence(const std::vector<int> &main_tour, const std
     {
         int edge_end = (edge_start + 1) % main_tour_size;
         if (min_edge_start == -1 ||
-            (distances[sub_sequence.back()][main_tour[edge_start]] + distances[sub_sequence.front()][main_tour[edge_end]] - distances[main_tour[edge_start]][main_tour[edge_end]] < min_distance))
+            (distances[subsequence.back()][main_tour[edge_start]] + distances[subsequence.front()][main_tour[edge_end]] - distances[main_tour[edge_start]][main_tour[edge_end]] < min_distance))
         {
             min_edge_start = edge_start;
             min_edge_end = edge_end;
-            min_distance = distances[sub_sequence.back()][main_tour[min_edge_start]] + distances[sub_sequence.front()][main_tour[min_edge_end]] - distances[main_tour[min_edge_start]][main_tour[min_edge_end]];
+            min_distance = distances[subsequence.back()][main_tour[min_edge_start]] + distances[subsequence.front()][main_tour[min_edge_end]] - distances[main_tour[min_edge_start]][main_tour[min_edge_end]];
             reverses_subsequence = true;
         }
 
         if (min_edge_start == -1 ||
-            (distances[sub_sequence.front()][main_tour[edge_start]] + distances[sub_sequence.back()][main_tour[edge_end]] - distances[main_tour[edge_start]][main_tour[edge_end]] < min_distance))
+            (distances[subsequence.front()][main_tour[edge_start]] + distances[subsequence.back()][main_tour[edge_end]] - distances[main_tour[edge_start]][main_tour[edge_end]] < min_distance))
         {
             min_edge_start = edge_start;
             min_edge_end = edge_end;
-            min_distance = distances[sub_sequence.front()][main_tour[min_edge_start]] + distances[sub_sequence.back()][main_tour[min_edge_end]] - distances[main_tour[min_edge_start]][main_tour[min_edge_end]];
+            min_distance = distances[subsequence.front()][main_tour[min_edge_start]] + distances[subsequence.back()][main_tour[min_edge_end]] - distances[main_tour[min_edge_start]][main_tour[min_edge_end]];
             reverses_subsequence = false;
         }
     }
+    return InsertPlace(min_edge_end, reverses_subsequence);
+}
+// NOTE: index1 must be smaller than index2.
+std::vector<int> insert_subsequence(const std::vector<int> &main_tour, const std::vector<int> &subsequence, const int &min_edge_end, const bool &reverses_subsequence)
+{
+    int main_tour_size = main_tour.size();
+    std::vector<int> new_tour;
 
+    // TODO: Ask about auto [--, --] = ~.
     // Inserts the subsequence to the main tour.
 
     for (int i = 0; i < main_tour_size; ++i)
@@ -123,33 +140,49 @@ std::vector<int> insert_subsequence(const std::vector<int> &main_tour, const std
         {
             if (reverses_subsequence)
             {
-                for (int j = sub_sequence.size() - 1; j >= 0; --j)
+                for (int j = subsequence.size() - 1; j >= 0; --j)
                 {
-                    new_tour.push_back(sub_sequence[j]);
+                    new_tour.push_back(subsequence[j]);
                 }
             }
             else
             {
-                for (int j = 0; j < sub_sequence.size(); ++j)
+                for (int j = 0; j < subsequence.size(); ++j)
                 {
-                    new_tour.push_back(sub_sequence[j]);
+                    new_tour.push_back(subsequence[j]);
                 }
             }
         }
         new_tour.push_back(main_tour[i]);
     }
 
-    assert(check_tour(new_tour, num_of_cities));
+    // assert(check_tour(new_tour, num_of_cities));
     return new_tour;
 }
 
-std::vector<int> move_subsequence(const std::vector<int> &tour, const std::vector<std::vector<double>> &distances, const double &time_in_second)
+void cut_out_subsequence(const std::vector<int> &tour, std::vector<int> main_tour, std::vector<int> subsequence, std::pair<int, int> indices)
+{
+    int num_of_cities = tour.size();
+    for (int i = 0; i < num_of_cities; ++i)
+    {
+        if (i == indices.first)
+        {
+            for (; i < indices.second; ++i)
+            {
+                subsequence.push_back(tour[i]);
+            }
+        }
+        main_tour.push_back(tour[i]);
+    }
+    return;
+}
+
+std::vector<int> &move_subsequence(std::vector<int> &tour, const std::vector<std::vector<double>> &distances, const double &time_in_second)
 {
     int num_of_cities = distances.size();
 
     std::random_device seed_gen;
     std::mt19937 random_engine(seed_gen());
-    std::vector<int> new_tour(tour);
 
     // Choose two edges at random, and uncross them if they are crossed.
     std::time_t start = std::time(NULL);
@@ -158,23 +191,13 @@ std::vector<int> move_subsequence(const std::vector<int> &tour, const std::vecto
         // Choose index1 and index2 at random.
         std::pair<int, int> indices = gen_random_indices(num_of_cities, random_engine);
         std::vector<int> main_tour, subsequence;
-        for (int i = 0; i < num_of_cities; ++i)
-        {
-            if (i == indices.first)
-            {
-                for (; i < indices.second; ++i)
-                {
-                    subsequence.push_back(new_tour[i]);
-                }
-            }
-            main_tour.push_back(new_tour[i]);
-        }
-
-        new_tour = insert_subsequence(main_tour, subsequence, distances);
+        cut_out_subsequence(tour, main_tour, subsequence, indices);
+        InsertPlace insert = get_place_to_insert_subsequence(main_tour, subsequence, distances);
+        tour = insert_subsequence(main_tour, subsequence, insert.index, insert.reverses);
     }
 
-    assert(check_tour(new_tour, num_of_cities));
-    return new_tour;
+    assert(check_tour(tour, num_of_cities));
+    return tour;
 }
 
 // Calculates the shortest tour to visit all the cities and return to the start.
@@ -196,24 +219,24 @@ std::vector<int> get_the_shortest_tour(const std::vector<std::vector<double>> &d
     return shortest_tour;
 }
 
-void test()
-{
-    // Input
-    std::vector<City> cities = read_input("input_0.csv");
-    assert(cities.size() == 5);
-    assert(cities[1].x == 1222.0393903625825);
-    assert(cities[1].y == 229.56212316547953);
+// void test()
+// {
+//     // Input
+//     std::vector<City> cities = read_input("input_0.csv");
+//     assert(cities.size() == 5);
+//     assert(cities[1].x == 1222.0393903625825);
+//     assert(cities[1].y == 229.56212316547953);
 
-    // Calculating distances
-    std::vector<std::vector<double>> distances = get_distances({City(0, 0), City(0, 1), City(1, 0), City(1, 1)});
-    assert(abs(distances[0][3] * distances[0][3] - 2.0) < 1e-4);
-    assert(distances[1][1] == 0);
+//     // Calculating distances
+//     std::vector<std::vector<double>> distances = get_distances({City(0, 0), City(0, 1), City(1, 0), City(1, 1)});
+//     assert(abs(distances[0][3] * distances[0][3] - 2.0) < 1e-4);
+//     assert(distances[1][1] == 0);
 
-    // Test for uncross_edges()
-    std::vector<int> uncrossed_tour = uncross_edges({0, 1, 2, 3}, 1, 3, distances);
-    std::vector<int> answer = std::vector<int>{0, 1, 3, 2};
-    assert(uncrossed_tour == answer);
-}
+//     // Test for uncross_edges()
+//     std::vector<int> uncrossed_tour = judge_uncross({0, 1, 2, 3}, 1, 3, distances);
+//     std::vector<int> answer = std::vector<int>{0, 1, 3, 2};
+//     assert(uncrossed_tour == answer);
+// }
 
 int main(int argc, char *argv[])
 {
